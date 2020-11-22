a78f8d7c0b66150e2e0c94f86ad85a305c00c423
rahul lunagariya <rahul.lunagariya132@gmail.com>
2020-11-22 18:05:54 +0530

    auth initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Header} from './components/common';
+import firebase from 'firebase';
+
+class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAQcaogmYXWqACW1QIhHOs29aBxPMwkd5U',
+      authDomain: 'authentication-68d72.firebaseapp.com',
+      databaseURL: 'https://authentication-68d72.firebaseio.com',
+      projectId: 'authentication-68d72',
+      storageBucket: 'authentication-68d72.appspot.com',
+      messagingSenderId: '840313907111',
+      appId: '1:840313907111:web:79c6861d338d8e4a0af882',
+      measurementId: 'G-JY98SRKEWJ',
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>An App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
